767ba8a57dc96251faeb631bcda514eb5c9dc8e2
Gustavo Bender <gustavo.benderr@gmail.com>
2023-06-15 21:44:13 -0300

    Changing the declaration of the functions to use the MAP struct

--- cgl/cgl.h
+++ cgl/cgl.h
@@ -14,9 +14,9 @@ typedef struct map MAP;
 
 
 
-void fill_map(int mapa[], size_t l, size_t c);
-void print_mapa(int mapa[], size_t l, size_t c);
-void split_maps(int **mapa1, int**mapa2);
-void next_state(int mapa1[], int mapa2[], size_t l, size_t c);
+void fill_map(MAP *map);
+void print_mapa(MAP *map);
+void split_maps(MAP *map1, MAP *map2);
+void next_state(MAP *map1, MAP *map2);
 MAP read_map(char name_archive[]);
 #endif // !_CGL_H_
