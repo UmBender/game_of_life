09569184b9bb2b4ece8f8807d8eabd918ce42fd5
Gustavo Bender <gustavo.benderr@gmail.com>
2023-06-15 21:44:27 -0300

    Changing the definition of the functions to use the MAP struct

--- cgl/cgl.c
+++ cgl/cgl.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include "cgl.h"
 
-void fill_map(int mapa[], size_t l, size_t c) {
-	for (size_t i = 0; i < l; i++) {
-		for (int j = 0; j < c; j++) {
-			mapa[i * c + j] = 0;
+void fill_map(MAP *map) {
+	for (size_t i = 0; i < map->lines; i++) {
+		for (int j = 0; j < map->columns; j++) {
+			map->grid[i * map->columns + j] = 0;
 			if((rand() %100) > 85) {
-				mapa[i * c + j] = 1;
+				map->grid[i * map->columns + j]= 1;
 			}
 		}
 	}
 }
 
-void print_mapa(int mapa[], size_t l, size_t c) {
-	for (size_t i = 0; i < l; i++) {
-		for (int j = 0; j < c; j++) {
-			if(mapa[i * c + j]){
+void print_mapa(MAP *map) {
+	for (size_t i = 0; i < map->lines; i++) {
+		for (int j = 0; j < map->columns; j++) {
+			if(map->grid[i * map->columns + j]){
 
 				printf("#");
 			}else{
@@ -28,44 +28,43 @@ void print_mapa(int mapa[], size_t l, size_t c) {
 	}
 }
 
-void split_maps(int **mapa1, int**mapa2) {
-	int **h; 
-	*h = *mapa1;
-	*mapa1 = *mapa2;
-	*mapa2 = *h;
-
+void split_maps(MAP *map1, MAP *map2) {
+	int *h;
+	h = map1->grid;
+	map1->grid = map2->grid;
+	map2->grid = h;
 }
 
-void next_state(int mapa1[], int mapa2[], size_t l, size_t c) {
-	for (int i = 0; i < l; i++) {
-		for (int j = 0; j < c; j++) {
-			int cell_state = mapa1[i * c + j];
+void next_state(MAP *map1, MAP *map2) {
+	for (int i = 0; i < map1->lines; i++) {
+		for (int j = 0; j < map1->columns; j++) {
+			int cell_state = map1->grid[i * map1->columns + j];
 			int sum_around = 0;
 			for (int li = -1; li < 2; li++) {
-				if(i + li >= 0 && i + li < l) {
+				if(i + li >= 0 && i + li < map1->lines) {
 					for (int lj = -1; lj  < 2; lj++) {
-						if (j + lj >= 0 && j + lj < c) {
-							sum_around += mapa1[(i + li) *c + j +lj];
+						if (j + lj >= 0 && j + lj < map1->columns) {
+							sum_around +=map1->grid[(i + li) *map1->columns + j +lj];
 						}
 					}
 				}
 			}
-			sum_around -= mapa1[i * c + j];
-			int pos = i *c + j;
-			mapa2[pos] = 0;
-			switch (mapa1[pos] % 2) {
+			sum_around -= map1->grid[i * map1->columns + j];
+			int pos = i *map1->columns + j;
+			map2->grid[pos] = 0;
+			switch (map1->grid[pos] % 2) {
 				case 0: 
 					if( sum_around == 3) {
-						mapa2[pos] = 1;
+						map2->grid[pos] = 1;
 					}else {
-						mapa2[pos] = 0;
+						map2->grid[pos] = 0;
 					}
 					break;
 				case 1:
 					if(sum_around == 2 || sum_around == 3) {
-						mapa2[pos] = 1;
+						map2->grid[pos] = 1;
 					}else {
-						mapa2[pos] = 0;
+						map2->grid[pos] = 0;
 					}
 
 					break;
@@ -103,8 +102,6 @@ MAP read_map(char archive[]) {
 
 	fclose(f);
 	return new_map;
-
-
 }
 
 
