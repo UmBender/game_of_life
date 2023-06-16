fb3ed92b55a504c55e3eb0b2e73b0d94441f61e5
Gustavo Bender <gustavo.benderr@gmail.com>
2023-06-15 21:27:05 -0300

    Definition of the struct MAP and of the function to read the MAP's

--- cgl/cgl.h
+++ cgl/cgl.h
@@ -3,9 +3,20 @@
 
 #include <stdlib.h>
 
+
+struct map {
+	int* grid;
+	size_t lines;
+	size_t columns;
+};
+
+typedef struct map MAP;
+
+
+
 void fill_map(int mapa[], size_t l, size_t c);
 void print_mapa(int mapa[], size_t l, size_t c);
 void split_maps(int **mapa1, int**mapa2);
 void next_state(int mapa1[], int mapa2[], size_t l, size_t c);
-
+MAP read_map(char name_archive[]);
 #endif // !_CGL_H_
