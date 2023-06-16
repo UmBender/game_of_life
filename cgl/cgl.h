30046f6907feb785e9e93f1d73b4e97f24006000
Gustavo Bender <gustavo.benderr@gmail.com>
2023-06-15 21:53:31 -0300

    Declaring the function to create a alloc a MAP with some size

--- cgl/cgl.h
+++ cgl/cgl.h
@@ -1,6 +1,7 @@
 #ifndef _CGL_H_
 #define _CGL_H_
 
+#include <stddef.h>
 #include <stdlib.h>
 
 
@@ -19,4 +20,5 @@ void print_mapa(MAP *map);
 void split_maps(MAP *map1, MAP *map2);
 void next_state(MAP *map1, MAP *map2);
 MAP read_map(char name_archive[]);
+MAP create_map(size_t lines, size_t columns);
 #endif // !_CGL_H_
